9f8574aeb9160f9abc2c19646a37dc722d31befe
kaustubh <kaustubh.khulbe@gmail.com>
2020-06-25 11:01:38 -0700

    Can request jobs

--- app/src/main/java/com/example/oddjobs/succesfullyRequested.java
+++ app/src/main/java/com/example/oddjobs/succesfullyRequested.java
@@ -2,13 +2,27 @@ package com.example.oddjobs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class succesfullyRequested extends AppCompatActivity {
+
+    private Button returnToMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succesfully_requested);
+
+        returnToMenu = (Button)findViewById(R.id.menu);
+        returnToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(succesfullyRequested.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
